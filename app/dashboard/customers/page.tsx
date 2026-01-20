6cee2a188736bbc3d4bed641e7a81412c5496397
Keith Austine <keithaustine619@gmail.com>
2026-01-20 15:15:00 +0300

    Complete Project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,25 @@
-export default function page(){
-    return <p>Customers Page</p>
+import {fetchFilteredCustomers} from '@/app/lib/data';
+import {Suspense} from 'react';
+import {InvoicesTableSkeleton} from '@/app/ui/skeletons';
+import CustomersTable from '@/app/ui/customers/table';
+
+export const metadata = { 
+    title: 'Customers',
+};
+export default async function Page({
+    searchParams,
+}: {
+    searchParams: {
+        query?: string};
+}) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+
+    return(
+        <main>
+            <Suspense fallback={<InvoicesTableSkeleton />}>
+                <CustomersTable customers = {customers} />
+            </Suspense>
+        </main>
+    )
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,12 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Bevan } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({ 
   weight: ['400', '700'],
   subsets: ['latin'] 
 });
+export const bevan = Bevan({
+  weight: ['400'],
+  style: ['italic'],
+  subsets: ['latin']
+})
